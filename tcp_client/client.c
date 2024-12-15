e2090a53aa6c1ec64e2231baacb9320b281bfbf4
AzJRC <rodajrc@gmail.com>
2024-12-15 01:17:43 -0600

    Refactor of TCP Client to funcitonal style

--- tcp_client/client.c
+++ tcp_client/client.c
@@ -4,46 +4,16 @@
 
 // ./client {host} -> stdout(Header information)
 int main(int argc, char *argv[]) {	
-	
-//	if (argc < 2) {
-//		fprintf(stderr, "Usage: ./client {host}");
-//		return -1;
-//	};
-	
+		
 	// variables
-	int opt;
 	int version_ip;
 	char *host_ip;
-	
+		
 	// default values
-	version_ip = 4;
+	version_ip = DEF_VER_IP;
 	
-	// arg parser
-	// Man pages get_opt(3): An element of argv that starts with '-' (and is not exactly "-" or "--") is  an  option  element.
-	if (argc == 1) {
-		fprintf(stderr, "Usage: %s [-v ip_version] -h ip\n", argv[0]);
-		return -1;
-	}
-	while ((opt = getopt(argc, argv, "v::h:")) != -1) {
-		switch(opt) {
-			case 'v':
-				if (optarg == NULL) break;
-				version_ip = atoi(optarg);
-				break;
-			case 'h':
-				host_ip = optarg;
-				break;
-			default:
-				fprintf(stderr, "Usage: %s [-v ip_version] -h {ip}\n", argv[0]);
-				return -1;
-		}
-	}
-	if (version_ip != 4 && version_ip != 6) {
-		fprintf(stderr, "Invalid IP version: Supported values are 4 and 6.\n");
-		return -1;
-	}
-	if (!host_ip) {
-		fprintf(stderr, "Missing Host IP after -h.\n");
+	// argument parser
+	if (parse_arguments(argc, argv, &version_ip, &host_ip) == -1) {
 		return -1;
 	}
 
@@ -52,43 +22,20 @@ int main(int argc, char *argv[]) {
 		fprintf(stdout, "TCP v6 client not implemented yet");
 		return 0;
 	}
-
-	const int v4_domain = AF_INET;
-	const int v6_domain = AF_INET6;
-	const int tcp_sock = SOCK_STREAM;
-	const int proto = 0;
 	
 	char *data;
-	char buff[512];
 	
-	/*
-		Man pages socket(2) say A connection to another socket is created with a connect(2) call.  
-		struct sockaddr_in {
-			sa_family_t     sin_family;     AF_INET
-			in_port_t       sin_port;       Port number
-			struct in_addr  sin_addr;       IPv4 address
-		};
-
-		struct in_addr {
-			in_addr_t s_addr;
-		};
-	*/
-
-	struct sockaddr_in sock;
-	sock.sin_family = AF_INET;
-	sock.sin_port = htons(PORT);
-	sock.sin_addr.s_addr = inet_addr(host_ip);
 
 	// Create socket for endpoint commnunication
-	int s = socket(v4_domain, tcp_sock, proto);
-	if (s == -1) {
-		return printerr("socket", errno);
+	int sfd = create_socket(version_ip);
+	if (sfd == -1) {
+		return handle_errors("socket", errno);
 	}
 
-	// Connect to the socket	
-	if (connect(s, (struct sockaddr *)&sock, sizeof(sock)) == -1) {
-		close(s);
-		return printerr("connect", errno);
+	// Connect to the socket		
+	if (connect_to_host(sfd, host_ip, version_ip) == -1) { 
+		close(sfd);
+		return handle_errors("connect", errno);
 	}
 
 	/*
@@ -97,19 +44,14 @@ int main(int argc, char *argv[]) {
 		When a session has been completed a close(2) may be performed.
 	*/
 
-	data = "HEAD / HTTP/1.0\r\n\r\n";
+	data = HTTP_REQ;
 
-	if (write(s, data, strlen(data)) == -1) {
-		close(s);
-		return printerr("write", errno);
+	send_request(sfd, data);
+
+	char resp[BUFF_SIZE];
+	if (recv_response(sfd, resp, sizeof(resp)) == 0) {
+		printf("\n%s\n", resp);
 	}
-	
-	memset(buff, 0, sizeof(buff));  // make sure buff is clean
-	if (read(s, buff, sizeof(buff)-1) == -1) {
-		close(s);
-		return printerr("read", errno);
-	}
-	
-	printf("\n%s\n", buff);	
+
 	return 0;
 }
--- tcp_client/client.h
+++ tcp_client/client.h
@@ -13,10 +13,123 @@
 
 // Definitions
 #define PORT 80
+#define DEF_VER_IP 4
+#define BUFF_SIZE 512
+#define HTTP_REQ "HEAD / HTTP/1.0\r\n\r\n"
 
 
-// Methods
-int printerr(char *err_funct, int errno_code) {
+// function prototypes
+int parse_arguments(int argc, char *argv[], int *version_ip, char *host_ip[]);
+int create_socket(int version_ip);
+int _create_v4_socket();
+int _create_v6_socket();
+int connect_to_host(int sfd, const char *host_ip, int version_ip);
+int _connect_to_v4host(int sfd, const char *host_ip);
+int _connect_to_v6host(int sfd, const char *host_ip);
+int handle_errors(const char *err_fuct, int errno_code);
+int send_request(int sfd, const char *req);
+int recv_response(int sfd, char *buff, size_t buff_size);
+
+
+int parse_arguments(int argc, char *argv[], int *version_ip, char *host_ip[]) {
+		int opt;
+		if (argc == 1) {
+				fprintf(stderr, "Usage: %s [-v ip_version] -h ip\n", argv[0]);
+				return -1;	
+		}
+		while ((opt = getopt(argc, argv, "v::h:")) != -1) {
+			switch(opt) {
+				case 'v':
+					if (optarg == NULL) break;
+					*version_ip = atoi(optarg);
+					break;
+				case 'h':
+					*host_ip = optarg;
+					break;
+				default:
+					fprintf(stderr, "Usage: %s [-v ip_version] -h {ip}\n", argv[0]);
+					return -1;
+			}
+		}
+		if (*version_ip != 4 && *version_ip != 6) {
+			fprintf(stderr, "Invalid IP version: %i. Supported values are 4 and 6.\n", *version_ip);
+			return -1;
+		}
+		if (*host_ip == NULL) {
+			fprintf(stderr, "Missing Host IP after -h.\n");
+			return -1;
+		}
+
+		return 0;
+}
+
+
+int create_socket(int version_ip) {
+	switch(version_ip) {
+		case 4:
+			return _create_v4_socket();
+		case 6:
+			return _create_v6_socket();
+		default:
+			fprintf(stderr, "Invalid IP version: %i. Supported values are 4 and 6.\n", version_ip);
+			return -1;
+	}
+}
+
+
+int _create_v4_socket() {
+	return socket(AF_INET, SOCK_STREAM, 0);
+}
+
+
+int _create_v6_socket() {
+	fprintf(stdout, "TCP v6 client not implemented yet");
+	return -1;
+}
+
+
+int connect_to_host(int sfd, const char *host_ip, int version_ip) {
+	switch(version_ip) {
+		case 4:
+			return _connect_to_v4host(sfd, host_ip);
+		case 6:
+			return _connect_to_v6host(sfd, host_ip);
+		default:
+			fprintf(stderr, "Invalid IP version: %i. Supported values are 4 and 6.\n", version_ip);
+			return -1;
+	}	
+}
+
+
+int _connect_to_v4host(int sfd, const char *host_ip) {
+	/*
+		Man pages socket(2) say A connection to another socket is created with a connect(2) call.  
+		struct sockaddr_in {
+			sa_family_t     sin_family;     AF_INET
+			in_port_t       sin_port;       Port number
+			struct in_addr  sin_addr;       IPv4 address
+		};
+
+		struct in_addr {
+			in_addr_t s_addr;
+		};
+	*/
+	struct sockaddr_in sock;
+	sock.sin_family = AF_INET;
+	sock.sin_port = htons(PORT);
+	sock.sin_addr.s_addr = inet_addr(host_ip);
+	
+	return connect(sfd, (struct sockaddr *)&sock, sizeof(sock));
+}
+
+
+int _connect_to_v6host(int sfd, const char *host_ip) {
+	fprintf(stdout, "TCP v6 client not implemented yet");
+	return -1;
+}
+
+
+int handle_errors(const char *err_funct, int  errno_code) {
 		/*
 			Print the str name of an errno code.
 			Return: -1
@@ -26,3 +139,21 @@ int printerr(char *err_funct, int errno_code) {
 }
 
 
+int send_request(int sfd, const char *req) {
+	if (write(sfd, req, strlen(req)) == -1) {
+		close(sfd);
+		return handle_errors("write", errno);
+	}
+	return 0;
+}
+
+
+int recv_response(int sfd, char *buff, size_t buff_size) {
+	ssize_t bytes_read = read(sfd, buff, buff_size - 1);
+	if (bytes_read == -1) {
+		close(sfd);
+		return handle_errors("read", errno);
+	}
+	buff[bytes_read] = '\0';
+	return 0;
+}
